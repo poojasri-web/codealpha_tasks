8707e14999ac18d2248788365220781b2a5fae7b
poojasri-web <poojasribabubalarajan@gmail.com>
2026-01-13 13:35:21 +0530

    Add Task 3 Portfolio

---
+++ task3_portfolio/script.js
@@ -0,0 +1,13 @@
+// Simple fade-in effect
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+  card.style.opacity = 0;
+  card.style.transform = 'translateY(20px)';
+
+  setTimeout(() => {
+    card.style.transition = '0.6s';
+    card.style.opacity = 1;
+    card.style.transform = 'translateY(0)';
+  }, 200);
+});
